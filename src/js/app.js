a813dde5223b16143b801452d33c6f642c34a180
alirezamohammadi <alireza_6313@yahoo.com>
2017-08-11 10:37:53 +0430

    custom js files merged and included into source index.html file

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+"use strict";
+
+angular.module('RDash', ['ui.bootstrap', 'ui.router', 'ngCookies']);
+
+
+/**
+ * Route configuration for the RDash module.
+ */
+angular.module('RDash').config(['$stateProvider', '$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+    // For unmatched routes
+    $urlRouterProvider.otherwise('/');
+
+    // Application routes
+    $stateProvider
+        .state('index', {
+            url: '/',
+            templateUrl: 'templates/dashboard.html'
+        })
+        .state('tables', {
+            url: '/tables',
+            templateUrl: 'templates/tables.html'
+        });
+}
+]);
---
+++ src/js/controllers/controllers.js
@@ -0,0 +1,68 @@
+/**
+ * Alerts Controller
+ */
+
+angular
+    .module('RDash')
+    .controller('AlertsCtrl', ['$scope', AlertsCtrl]);
+
+function AlertsCtrl($scope) {
+    $scope.alerts = [{
+        type: 'success',
+        msg: 'Thanks for visiting! Feel free to create pull requests to improve the dashboard!'
+    }, {
+        type: 'danger',
+        msg: 'Found a bug? Create an issue with as many details as you can.'
+    }];
+
+    $scope.addAlert = function () {
+        $scope.alerts.push({
+            msg: 'Another alert!'
+        });
+    };
+
+    $scope.closeAlert = function (index) {
+        $scope.alerts.splice(index, 1);
+    };
+}
+
+
+/**
+ * Master Controller
+ */
+
+angular.module('RDash')
+    .controller('MasterCtrl', ['$scope', '$cookieStore', MasterCtrl]);
+
+function MasterCtrl($scope, $cookieStore) {
+    /**
+     * Sidebar Toggle & Cookie Control
+     */
+    var mobileView = 992;
+
+    $scope.getWidth = function () {
+        return window.innerWidth;
+    };
+
+    $scope.$watch($scope.getWidth, function (newValue, oldValue) {
+        if (newValue >= mobileView) {
+            if (angular.isDefined($cookieStore.get('toggle'))) {
+                $scope.toggle = !$cookieStore.get('toggle') ? false : true;
+            } else {
+                $scope.toggle = true;
+            }
+        } else {
+            $scope.toggle = false;
+        }
+
+    });
+
+    $scope.toggleSidebar = function () {
+        $scope.toggle = !$scope.toggle;
+        $cookieStore.put('toggle', $scope.toggle);
+    };
+
+    window.onresize = function () {
+        $scope.$apply();
+    };
+}
