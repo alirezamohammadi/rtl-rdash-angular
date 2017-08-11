a8b6136cdd178bfd2f954b459b67eb7586484d36
alirezamohammadi <alireza_6313@yahoo.com>
2017-08-11 21:32:17 +0430

    file formatted

--- src/js/app.js
+++ src/js/app.js
@@ -7,20 +7,20 @@ angular.module('RDash', ['ui.bootstrap', 'ui.router', 'ngCookies']);
  * Route configuration for the RDash module.
  */
 angular.module('RDash').config(['$stateProvider', '$urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
+    function ($stateProvider, $urlRouterProvider) {
 
-    // For unmatched routes
-    $urlRouterProvider.otherwise('/');
+        // For unmatched routes
+        $urlRouterProvider.otherwise('/');
 
-    // Application routes
-    $stateProvider
-        .state('index', {
-            url: '/',
-            templateUrl: 'templates/dashboard.html'
-        })
-        .state('tables', {
-            url: '/tables',
-            templateUrl: 'templates/tables.html'
-        });
-}
+        // Application routes
+        $stateProvider
+            .state('index', {
+                url: '/',
+                templateUrl: 'templates/dashboard.html'
+            })
+            .state('tables', {
+                url: '/tables',
+                templateUrl: 'templates/tables.html'
+            });
+    }
 ]);
