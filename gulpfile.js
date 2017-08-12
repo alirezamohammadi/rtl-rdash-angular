bca74c4f1d3f01213f309152190d9e3dfc9df24e
alirezamohammadi <alireza_6313@yahoo.com>
2017-08-12 22:10:48 +0430

    404 error for some fomt files fixed

--- gulpfile.js
+++ gulpfile.js
@@ -17,8 +17,8 @@ var paths = {
     images: 'src/img/**/*.*',
     templates: 'src/templates/**/*.html',
     index: 'src/index.html',
-    bower_fonts: 'src/components/**/*.{ttf,woff,eof,svg}',
-    ui_fonts: 'src/rdash-ui/**/*.{ttf,woff,eof,svg}'
+    bower_fonts: 'src/components/**/*.{ttf,woff,eof,svg,woff2}',
+    ui_fonts: 'src/rdash-ui/**/*.{ttf,woff,eof,svg,woff2}'
 };
 
 /**
