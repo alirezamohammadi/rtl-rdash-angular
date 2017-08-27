767f0ee1d648789d30378886edd8f4f36e097682
alirezamohammadi <alireza_6313@yahoo.com>
2017-08-27 15:25:58 +0430

    some old bs3 classes replaced with new bs4

--- src/js/directives/directives.js
+++ src/js/directives/directives.js
@@ -72,7 +72,7 @@ function rdWidgetTitle() {
             icon: '@'
         },
         transclude: true,
-        template: '<div class="widget-header"><div class="row"><div class="pull-left"><i class="fa" ng-class="icon"></i> {{title}} </div><div class="pull-right col-xs-6 col-sm-4" ng-transclude></div></div></div>',
+        template: '<div class="widget-header"><div class="row"><div class="float-left"><i class="fa" ng-class="icon"></i> {{title}} </div><div class="float-right col-xs-6 col-sm-4" ng-transclude></div></div></div>',
         restrict: 'E'
     };
     return directive;
